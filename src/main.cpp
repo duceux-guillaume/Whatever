bf0ac81869bb85cac0013bea8b591de7451c616d
Guillaume Duceux <guillaume.duceux@cuisinous.com>
2020-12-08 09:28:25 +0100

    create skeleton project that compiles

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "whatever/whatever.hpp"
+#include <iostream>
+
+int main(int argc, const char **argv) {
+  std::cout << "Hello world" << std::endl;
+
+  MyAwesomeStruct toto;
+  return 0;
+}
---
+++ src/whatever.cpp
@@ -0,0 +1,1 @@
+#include "whatever/whatever.hpp"
